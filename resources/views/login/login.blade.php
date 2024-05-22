6639f69e986fda7c136a0f3f8f145760b01de3d1
Jitendra <jitendrasahu17996@gmail.com>
2024-05-22 20:32:01 +0530

    login page

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,7 +1,6 @@
 @extends('login.loginlayout.loginlayout')
 @section('login-page')
     <div class="container">
-
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
@@ -38,17 +37,7 @@
                                             Login
                                         </a>
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
                                     </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
                                     <div class="text-center">
                                         <a class="small" href="register.html">Create an Account!</a>
                                     </div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'Auth\LoginController@index');
+
+Route::group(['middleware'=>['auth']] , function(){
+    /*Manage customer routes*/
+});
+
 Route::get('/', function () {
     return view('login/login');
 });
