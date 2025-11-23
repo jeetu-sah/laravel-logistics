2161c207b4d81e7c14c2ebdbd1cd22cea9e4b717
Jitendra sahu <jitu.cybertrons@gmail.com>
2025-11-24 01:07:54 +0530

    changes address and bank details

--- resources/views/admin/booking/edit.blade.php
+++ resources/views/admin/booking/edit.blade.php
@@ -55,7 +55,7 @@
                                         </div>
                                         <div class="col-md-6">
                                             <input type="text" value="{{ $booking->freight_amount }}" name="freight_amount"
-                                                id="freight_amount" class="form-control mb-1" readonly />
+                                                id="freight_amount" class="form-control mb-1" />
                                         </div>
 
                                         <!-- WBC -->
